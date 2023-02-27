ec25877ce7da373756c09a71e4dc2c0e6f788303
MarioAlves <mario.alves@sc.senai.br>
2023-02-27 06:35:38 -0300

    Update release files names

--- build_release.bat
+++ build_release.bat
@@ -1,2 +1,2 @@
-.venv\Scripts\pyinstaller.exe --noconsole --onefile sbfrm_gui.py;mv dist/sbfrm_gui releases/windows/sbfrm_gui
-.venv\Scripts\pyinstaller.exe --noconsole --onefile sbfrm.py;mv dist/sbfrm releases/windows/sbfrm
+.venv\Scripts\pyinstaller.exe --noconsole --onefile sbfrm_gui.py
+.venv\Scripts\pyinstaller.exe --noconsole --onefile sbfrm.py
--- build_release.sh
+++ build_release.sh
@@ -1,2 +1,2 @@
-pyinstaller --noconsole --onefile sbfrm_gui.py;mv dist/sbfrm_gui releases/linux/sbfrm_gui
+pyinstaller --noconsole --onefile sbfrm_gui.py;mv dist/sbfrm_gui releases/linux/sbfrm-gui
 pyinstaller --noconsole --onefile sbfrm.py;mv dist/sbfrm releases/linux/sbfrm
